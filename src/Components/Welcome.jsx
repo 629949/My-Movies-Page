b8d13a61fb7d84c7a586613175b1dda16dec85f1
GADOBUA <emmanuelobua54@gmail.com>
2024-05-15 19:56:37 +0300

    add image

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+ import React from 'react'
 
 function Welcome(props) {
   return (
     <div>
       <h1 className='flex text-white justify-center'>Welcome to my Ranking page</h1>
-      <img src="" alt="" />
+      <image className='flex w-500 justifiy-center'>
+        <img src=".\src\Images\Attack on Titan.Jpg" alt="movie tile" />
+      </image>
+      <p>Please feel free to browse through my cartaloge</p>
     </div>
   )
 }
