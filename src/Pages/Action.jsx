61659e7dec30490bbeb6eeb2f5a409b701482870
629949 <gademma54@gmail.com>
2024-05-26 00:01:28 +0300

    render lists

--- src/Pages/Action.jsx
+++ src/Pages/Action.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 
 function Action() {
+  const List =[
+    {
+      title:'John wick',
+      About:'This is good action',
+      Trailer:'Watch Trailer',
+    },
+    {
+      title:'John wick',
+      About:'This is good action',
+      Trailer:'Watch Trailer',
+    },
+    {
+      title:'John wick',
+      About:'This is good action',
+      Trailer:'Watch Trailer',
+    },
+    {
+      title:'John wick',
+      About:'This is good action',
+      Trailer:'Watch Trailer',
+    },
+    {
+      title:'John wick',
+      About:'This is good action',
+      Trailer:'Watch Trailer',
+    },
+  ]
+
+  const movieList = List.map((movie) => (
+    <div className='border'>
+    <h3>Title: {movie.title}</h3>
+    <p>About: {movie.About}</p>
+    <button>{movie.Trailer}</button>
+  </div>
+    
+  ))
   return (
-    <div className='bg-black'>
-     
-    </div>
+    <div >{movieList}</div>
   )
 }
 
